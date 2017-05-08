e24a432057f001d1efac8874c61fd06ccc4dbbdf
yuanyu90221 <yuanyu90221@gmail.com>
2017-05-09 01:06:35 +0800

    add Twitch project

---
+++ answers/hw4/README.md
@@ -0,0 +1,42 @@
+# 4 從假資料到真資料：Ajax 與 API 串接
++   1 Twitch API裡面有一個 API 是可以拿到現在正在直播的某個遊戲底下的資料，API 的描述是「Gets a list of live streams.」，
++     看到這行就代表你找對 API 了。
++   2 API 要帶的參數有一個 game 的欄位，請帶League%20of%20Legends
++   3 請顯示 20 個實況，不多不少，要剛好 20 個
+## 參考API
++   <https://dev.twitch.tv/docs/v5/reference/streams/#get-live-streams>
+## 使用Lib
++   1.JQuery 配置(先在.bowerrc檔案內定義 bower lib 安裝位置，使用`bower install jquery`安裝 jquery)
++   2.利用 $(document).ready 設定 DOM Ready之後 load data的邏輯
+
+<pre>
+    $(document).ready(function(){
+        // 自定義 DOM ready 邏輯
+    });
+</pre>
+
++   3.利用$.prepend這個method 將資料動態塞入資料最前方
+
+<pre>
+      $('.out_space').prepend($(`<div class="live_cell">
+                                    <img src="${view}"/>
+                                     <div class="player_part">
+                                        <div class="player_logo">
+                                            <img src="${logo_view}"/>
+                                        </div>
+                                        <div class="player_text">
+                                            <span>${display_name}</span>
+                                            <span>${name}</span>
+                                        </div>
+                                    </div> 
+                               </div>`));
+</pre>
+
++   4.利用$.children.length 算出當下子元素的個數
+
+<pre>
+    $('.out_space').children().length;
+</pre>
+
+##  github.io 連結 
++   <https://yuanyu90221.github.io/frontend-intermediate-course/answers/hw4/> 
---
+++ answers/hw4/assets/js/loadTwitch.js
@@ -0,0 +1,70 @@
+'use strict'
+$(document).ready(function(){
+    loadTwitch();
+});
+/**
+ * 動態載入 Twitch API 個數
+ */
+function loadTwitch(){
+    // 參考API ref https://dev.twitch.tv/docs/v5/reference/streams/#get-live-streams
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&limit=20',
+        headers: {
+        'Client-ID': '8ussvz4lfocpyaewqd7f9mmso7t4kj'
+        },
+        success: function(data) {
+            // 取出 data 所需的streams arrray
+            appendDataToDOM(data.streams);
+        }
+    });
+}
+/**
+ * 把Twitch API 取得的 data array動態匯出
+ * 
+ * @param {any} data
+ */
+function appendDataToDOM(data){ 
+  // 在 append 之前取的當下 .out_space下子節點個數
+  var current_channel_num = getRenderChannelNums();
+  // 這邊做了一個判端式用來處理 reload的時候不重複加入多超過20 +2(2個空結構用來預防跑版) 
+  if(current_channel_num < 22){
+      if(data&&data.length > 0 ){
+          data.forEach((data)=>{
+              appendData(data);
+          });
+      }
+  }
+}
+/**
+ * 取得當下 out_space內的 第一層子節點個數
+ * 
+ * @returns out_space內的 第一層子節點個數
+ */
+function getRenderChannelNums(){
+    return $('.out_space').children().length;
+}
+/**
+ * 把stream物件動態匯出來
+ * 
+ * @param {any} streamObj stream的物件
+ */
+function appendData(streamObj){
+    let {preview, channel} = streamObj;
+    let {medium,large, small} = preview;
+    let view = (medium!==null)?medium:(small!==null)?small:(large!==null)?large:"./assets/images/view-default.jpg"; 
+    let {logo, name, display_name} = channel;
+    let logo_view = (logo!==null)?logo:"./assets/images/player-default.png";
+    $('.out_space').prepend($(`<div class="live_cell">
+                                    <img src="${view}"/>
+                                     <div class="player_part">
+                                        <div class="player_logo">
+                                            <img src="${logo_view}"/>
+                                        </div>
+                                        <div class="player_text">
+                                            <span>${display_name}</span>
+                                            <span>${name}</span>
+                                        </div>
+                                    </div> 
+                               </div>`));
+}
