2cfe7e44bc4edad64fa2441cdd6ec6a67ab953fe
yuanyu90221 <yuanyu90221@gmail.com>
2017-06-19 15:12:45 +0800

    把設定檔語法也用semistandard檢查

--- answers/hw10/webpack.config.js
+++ answers/hw10/webpack.config.js
@@ -1,34 +1,32 @@
 let webpack = require('webpack');
-let uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+let UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 let path = require('path');
 module.exports = {
-    // 起始的 js
-    entry: './assets/js/loadTwitch.js',
-    // 輸出位址
-    output: {
-        path: path.join(__dirname,'assets/js'),
-        filename: 'bundle.min.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loaders: ['babel-loader?presets[]=es2015']
-            },
-            {
-                test: /\.json$/,
-                use: 'json-loader'
-            }
-            
-        ]
-    },
-    // Plugins
-    plugins: [
-        new uglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
+  // 起始的 js
+  entry: './assets/js/loadTwitch.js',
+  // 輸出位址
+  output: {
+    path: path.join(__dirname, 'assets/js'),
+    filename: 'bundle.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader?presets[]=es2015']
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      }
     ]
-    
-}
+  },
+  // Plugins
+  plugins: [
+    new UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
