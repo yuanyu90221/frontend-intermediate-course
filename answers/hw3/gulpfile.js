42d4a3b3aada3f55ca9ce20cf0cf296f0221832f
yuanyu90221 <yuanyu90221@gmail.com>
2017-06-28 14:35:54 +0800

    add gulp-autoprefixer to hw3
    把設定檔語法也用semistandard檢查

--- answers/hw3/gulpfile.js
+++ answers/hw3/gulpfile.js
@@ -1,10 +1,11 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var autoprefixer = require('gulp-autoprefixer');
 gulp.task('sass', function() {
     gulp.src('./assets/scss/**/*.scss')
         .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./assets/css/'))
 });
 
